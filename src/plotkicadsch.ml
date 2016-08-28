0610c5004ab561f7a1fc0dc20498455fe7461440
Jean-Noel Avila <jn.avila@free.fr>
2016-08-28 17:13:37 +0200

    add command line parsing

--- src/plotkicadsch.ml
+++ src/plotkicadsch.ml
@@ -8,14 +8,19 @@ let stream_fold f stream init =
     stream;
   !result
 
-let _ =
-  let init = initial_context () in
-  let lib = "/home/jnavila/Developpement/kicad/kicad-library-utils/sch/complex_hierarchy/complex_hierarchy-cache.lib" in
-  let with_lib = add_lib lib init in
-  let ic = open_in "/home/jnavila/Developpement/kicad/kicad-library-utils/sch/complex_hierarchy/complex_hierarchy.sch" in
-  let oc = open_out "../electric/complex.svg" in
-  let ss () = Stream.from (fun _ -> try Some (input_line ic) with _ -> None) in
-  let endcontext = stream_fold parse_line  (ss ()) with_lib in
-  output_context endcontext oc;
-  close_out oc;
-  close_in ic
+let () =
+  let init = ref (initial_context ()) in
+  let speclist = [
+      ("-l", Arg.String (fun lib -> init := add_lib lib !init ), "specify component library");
+      ("-f", Arg.String(fun sch ->
+               let fileout = sch ^ ".svg" in
+               let ic = open_in sch in
+               let oc = open_out fileout in
+               let ss () = Stream.from (fun _ -> try Some (input_line ic) with _ -> None) in
+               let endcontext = stream_fold parse_line  (ss ()) !init in
+               output_context endcontext oc;
+               Printf.printf "processing %s to %s\n" sch fileout;
+               close_out oc;
+               close_in ic), "sch file to process")] in
+  let usage_msg = "plotkicadsch prints Kicad sch files to svg" in
+  Arg.parse speclist print_endline usage_msg
