e3f9917785b532adfad5a09756cbcf5a9531eb5e
Jean-Noel Avila <jn.avila@free.fr>
2017-12-24 12:13:26 +0100

    Functorize the differ of schematics

--- plotkicadsch/src/listPainter.ml
+++ plotkicadsch/src/listPainter.ml
@@ -1,58 +1,6 @@
 open Kicadsch.Sigs
 
 type image_data = Buffer.t
-(*
-let hash_fold_style hsv (arg:style) =
-  match arg with
-  | Bold -> Hash.fold_int hsv 0
-  | Italic -> Hash.fold_int hsv 1
-  | BoldItalic -> Hash.fold_int hsv 2
-  | NoStyle -> Hash.fold_int hsv 3
-
-let hash_fold_justify hsv (arg:justify) =
-  match arg with
-  | J_left -> Hash.fold_int hsv 0
-  | J_right -> Hash.fold_int hsv 1
-  | J_center -> Hash.fold_int hsv 2
-  | J_bottom -> Hash.fold_int hsv 3
-  | J_top -> Hash.fold_int hsv 4
-
-let hash_fold_size hsv (Size arg: size) =
-  Hash.fold_int hsv arg
-
-let hash_fold_coord hsv (Coord (x,y)) =
-  Hash.fold_int (Hash.fold_int hsv x) y
-
-let hash_fold_orientation hsv (arg: orientation) =
-  match arg with
-  | Orient_H -> Hash.fold_int hsv 0
-  | Orient_V -> Hash.fold_int hsv 1
-
-let hash_fold_kolor hsv = function
-  | NoColor -> Hash.fold_int hsv 0
-  | Black -> Hash.fold_int hsv 1
-  | Green -> Hash.fold_int hsv 2
-  | Red -> Hash.fold_int hsv 3
-  | Blue -> Hash.fold_int hsv 4
-  | Brown -> Hash.fold_int hsv 5
-
-let hash_fold_image_data hsv _ = hsv
-
-let g_compare a b = if a=b then 0 else 1
-
-let compare_kolor = g_compare
-let compare_style = g_compare
-let compare_justify = g_compare
-let compare_orientation = g_compare
-let compare_size = g_compare
-let compare_coord= g_compare
-let compare_string = String.compare
-let compare_int = g_compare
-let compare_float = g_compare
-let compare_image_data _ _ = 0
-
-let sexp_of_t _ = Sexplib0.Sexp.Atom "test"
-                  *)
 type t =
   | Text of kolor*string*orientation*coord*size*justify*style
   | Line of kolor*size*coord*coord
@@ -61,17 +9,12 @@ type t =
   | Arc of kolor*coord*coord*int
   | Image of coord*float*image_data
   | Format of coord
-(*                           [@@deriving hash, compare]
-
-let t_of_sexp _ = Line (Black, Size 0, Coord (0, 0), Coord (0,0))
- *)
 
 type listcanevas = t list
 
-
-module ListPainter = Kicadsch.MakeSchPainter( struct
-
+module L =  struct
     type t = listcanevas
+    type painterContext = listcanevas
 
     let paint_text ?(kolor=Black) text (o:orientation) coords s j stl ctx =
       Text (kolor, text, o, coords, s, j, stl) :: ctx
@@ -88,4 +31,4 @@ module ListPainter = Kicadsch.MakeSchPainter( struct
     let get_context () = []
     let set_canevas_size x y c = (Format (Coord (x, y))) :: c
 
-  end)
+  end
--- plotkicadsch/src/plotgitsch.ml
+++ plotkicadsch/src/plotgitsch.ml
@@ -2,8 +2,8 @@ open Lwt.Infix
 open Git_unix
 module Search = Git.Search.Make(FS)
 
-module SvgSchPainter = Kicadsch.MakeSchPainter(SvgPainter)
-open SvgSchPainter
+module S = Kicadsch.MakeSchPainter(SvgPainter)
+open Kicadsch.Sigs
 
 module type Simple_FS = sig
   val get_content: string list -> string Lwt.t
@@ -14,49 +14,49 @@ exception InternalGitError of string
 
 let git_fs theref =
   (module
-     struct
-       let fs = FS.create ~root:(Sys.getcwd ()) ()
-       let theref = theref
-       let with_path path action =
-         fs >>= fun t ->
-         theref >>= fun h ->
-         Search.find t h (`Commit(`Path path)) >>= function
+  struct
+    let fs = FS.create ~root:(Sys.getcwd ()) ()
+    let theref = theref
+    let with_path path action =
+      fs >>= fun t ->
+      theref >>= fun h ->
+      Search.find t h (`Commit(`Path path)) >>= function
          | None     -> Lwt.fail(InternalGitError ("path not found: /" ^ (String.concat "/" path)))
          | Some sha ->
-            FS.read t sha >>= function
-            | Some a -> action a
-            | None -> Lwt.fail (InternalGitError "sha not found")
+           FS.read t sha >>= function
+           | Some a -> action a
+           | None -> Lwt.fail (InternalGitError "sha not found")
 
-       let get_content filename =
-         with_path filename @@ function
-                              | Git.Value.Blob b -> Lwt.return (Git.Blob.to_raw b)
-                              | _ -> Lwt.fail(InternalGitError "not a valid path")
-       let find_file filter t =
-         let open Git.Tree in
-         t |>
-           List.filter (fun e -> filter e.name) |>
-           List.map (fun e -> e.name, (Git.Hash.to_hex e.node))
+    let get_content filename =
+      with_path filename @@ function
+      | Git.Value.Blob b -> Lwt.return (Git.Blob.to_raw b)
+      | _ -> Lwt.fail(InternalGitError "not a valid path")
+    let find_file filter t =
+      let open Git.Tree in
+      t |>
+      List.filter (fun e -> filter e.name) |>
+      List.map (fun e -> e.name, (Git.Hash.to_hex e.node))
 
-       let list_files pattern =
-         with_path [] @@ function
-                        | Git.Value.Tree t -> Lwt.return @@ find_file pattern t
-                        | _ -> Lwt.fail (InternalGitError "not a tree!")
+    let list_files pattern =
+      with_path [] @@ function
+      | Git.Value.Tree t -> Lwt.return @@ find_file pattern t
+      | _ -> Lwt.fail (InternalGitError "not a tree!")
   end: Simple_FS)
 
-
-
-let true_fs rootname = (module struct
+let true_fs rootname =
+  (module
+  struct
     let rootname = rootname
     let get_content filename = Lwt_io.with_file ~mode:Lwt_io.input (String.concat "/" filename) (fun x -> Lwt_io.read x)
     let hash_file filename = get_content [filename] >|= fun c ->
-                             let blob_content = (Printf.sprintf "blob %d\000" (String.length c)) ^ c in
+      let blob_content = (Printf.sprintf "blob %d\000" (String.length c)) ^ c in
                              filename, (Sha1.to_hex (Sha1.string blob_content))
     let list_files pattern =
       let all_files  = (Lwt_unix.files_of_directory rootname) in
       let matched_files = Lwt_stream.filter pattern all_files in
       let decorated_files = Lwt_stream.map_s (fun name -> (hash_file name)) matched_files in
       Lwt_stream.to_list decorated_files
-end: Simple_FS)
+  end: Simple_FS)
 
 let ends_with s e =
   let ls = String.length s in
@@ -71,53 +71,45 @@ let ends_with s e =
     in
     loop s e 0
 
-let find_libs thefs =
-  let module M = (val thefs: Simple_FS) in
-  M.list_files (fun name -> ends_with name "-cache.lib")  >|= List.map (fun (n, _) -> n)
 
-let find_schematics thefs =
-    let module M = (val thefs: Simple_FS) in
-  M.list_files (fun name -> ends_with name ".sch")
+module L = Kicadsch.MakeSchPainter(ListPainter.L)
+module LP = struct
+  include L
+  type painterContext=ListPainter.listcanevas
+end
 
-let read_libs _ thefs lib_list  =
-  let module M = (val thefs: Simple_FS) in
+module FSPainter (S: SchPainter) (F: Simple_FS) : sig
+  val find_schematics: unit -> (string*string) list Lwt.t
+  val process_file: S.schContext Lwt.t -> string -> S.painterContext Lwt.t
+  val context_from: unit -> S.schContext Lwt.t
+end =
+struct
+  let find_schematics () = F.list_files (fun name -> ends_with name ".sch")
+  let process_file initctx filename =
+    let parse c l = S.parse_line l c in
+    initctx >>= fun init ->
+    F.get_content [filename] >|= fun ctt ->
+    let lines = String.split_on_char '\n' ctt in
+    let endctx = List.fold_left parse init lines in
+    S.output_context endctx
+
+  let find_libs () =
+    F.list_files (fun name -> ends_with name "-cache.lib")  >|= List.map (fun (n, _) -> n)
+
+  let read_libs _ lib_list  =
   Lwt_list.fold_left_s (fun c l ->
-      M.get_content [l] >|=
+      F.get_content [l] >|=
       Str.split (Str.regexp "\n") >|=
-      List.fold_left (fun ctxt l -> add_lib l ctxt) c) (initial_context () ) lib_list
+      List.fold_left (fun ctxt l -> S.add_lib l ctxt) c) (S.initial_context () ) lib_list
 
-module L = ListPainter.ListPainter
-
-let read_libs_l _ thefs lib_list  =
-  let module M = (val thefs: Simple_FS) in
-  Lwt_list.fold_left_s (fun c l ->
-      M.get_content [l] >|=
-      Str.split (Str.regexp "\n") >|=
-      List.fold_left (fun ctxt l -> L.add_lib l ctxt) c) (L.initial_context () ) lib_list
+  let context_from () = find_libs () >>= read_libs (S.initial_context ())
+end
 
 let intersect_lists l1l l2l =
   l1l >>= fun l1 ->
   l2l >|= fun l2 -> (
   List.filter (fun (name2, sha2) -> (List.exists (fun (name1, sha1) -> ((String.equal name1 name2) && (not(String.equal sha2 sha1)))) l1)) l2 |>
     List.map (fun (n, _) -> n))
-
-
-
-let process_file initctx svg_name content =
-  initctx >>= fun init ->
-  content >|= Str.split (Str.regexp "\n") >>= fun lines ->
-  Lwt_stream.fold parse_line (Lwt_stream.of_list lines) init >>= fun endcontext ->
-  Lwt_io.with_file ~mode:Lwt_io.Output svg_name (fun o -> Lwt_io.write o (SvgPainter.write @@ output_context endcontext))
-
-
-let context_list initctx content =
-  let parse c l = L.parse_line l c in
-  initctx >>= fun init ->
-  content >|= fun ctt ->
-  let lines = String.split_on_char '\n' ctt in
-  let endctx = List.fold_left parse init lines in
-  let endcontext = L.output_context endctx in
-  Array.of_list endcontext
 
 let rev_parse r =
   let open Lwt_process in
@@ -149,27 +141,27 @@ let plot_elt style (arg: ListPainter.t) out_ctx=
   ListPainter.(
     let module O = SvgPainter in
     let kolor = match style with
-      | Theirs -> Printf.printf "printing their ";Kicadsch.Sigs.Red
-      | Ours -> Printf.printf "printing our ";Kicadsch.Sigs.Green
-      | Idem -> Printf.printf "printing idem ";Kicadsch.Sigs.Black in
+      | Theirs -> Kicadsch.Sigs.Red
+      | Ours -> Kicadsch.Sigs.Green
+      | Idem -> Kicadsch.Sigs.Black in
     match arg with
-  | Text (_, text, o, c, s, j, style) -> Printf.printf "text %s\n" text;O.paint_text ~kolor text o c s j style out_ctx
-  | Line (_, s, from_, to_) -> Printf.printf "line\n";O.paint_line ~kolor ~width:s from_ to_ out_ctx
-  | Rect (_,  c1, c2) -> Printf.printf "rect\n";O.paint_rect c1 c2 out_ctx
-  | Circle (_, center, radius) -> Printf.printf "circle\n";O.paint_circle center radius out_ctx
-  | Arc (_ ,start_, end_, radius) -> Printf.printf "arc\n";O.paint_arc start_ end_ radius out_ctx
-  | Image (corner, scale , data) -> Printf.printf "image\n";O.paint_image corner scale data out_ctx
-  | Format (Coord (x, y)) -> Printf.printf "format %d %d\n" x y;O.set_canevas_size x y out_ctx
+  | Text (_, text, o, c, s, j, style) -> O.paint_text ~kolor text o c s j style out_ctx
+  | Line (_, s, from_, to_) -> O.paint_line ~kolor ~width:s from_ to_ out_ctx
+  | Rect (_,  c1, c2) -> O.paint_rect c1 c2 out_ctx
+  | Circle (_, center, radius) -> O.paint_circle center radius out_ctx
+  | Arc (_ ,start_, end_, radius) -> O.paint_arc start_ end_ radius out_ctx
+  | Image (corner, scale , data) -> O.paint_image corner scale data out_ctx
+  | Format (Coord (x, y)) -> O.set_canevas_size x y out_ctx
   )
 
 let draw_range ctx r = Patience_diff_lib.Patience_diff.Range.(
-    Printf.printf "Printing range ";
     match r with
-    | Same a -> Printf.printf "Same\n";Array.fold_right (fun (x, _) -> plot_elt Idem x) a ctx
-    | Old a  -> Printf.printf "Old\n"; Array.fold_right (plot_elt Theirs) a ctx
-    | New a  -> Printf.printf "New\n"; Array.fold_right (plot_elt Ours) a ctx
-    | Replace (n,o) -> Printf.printf "Replace\n";Array.fold_right (plot_elt Ours) n ctx |> Array.fold_right (plot_elt Theirs) o
-    | Unified a -> Printf.printf "Unified\n";Array.fold_right (plot_elt Idem) a ctx
+    | Same a -> Array.fold_right (fun (x, _) -> plot_elt Idem x) a ctx
+    | Old a  ->  Array.fold_right (plot_elt Theirs) a ctx
+    | New a  ->  Array.fold_right (plot_elt Ours) a ctx
+    | Replace (n,o) -> Array.fold_right (plot_elt Ours) n ctx |>
+                       Array.fold_right (plot_elt Theirs) o
+    | Unified a -> Array.fold_right (plot_elt Idem) a ctx
                        )
 type hunk = ListPainter.t Patience_diff_lib.Patience_diff.Hunk.t
 
@@ -179,10 +171,8 @@ let draw_hunk (h: hunk) ctx =
 let draw_difftotal other mine out_canevas =
   let comparison = Patdiff.get_hunks ~transform ~context:5 ~mine ~other in
   let draw_all_hunk (ctx, n) (h: hunk) =
-    Printf.printf "plotting common from %d to %d over %d\n" n h.mine_start (Array.length mine - 1);
-    (Array.fold_right (plot_elt Idem) (Array.sub mine n (h.mine_start - n - 1)) ctx|> draw_hunk h) , (h.mine_start + h.mine_size - 2) in
+    (Array.fold_right (plot_elt Idem) (Array.sub mine n (h.mine_start - n - 1)) ctx|>draw_hunk h) , (h.mine_start + h.mine_size - 2) in
   let ctx, n = List.fold_left draw_all_hunk (out_canevas, 0) comparison in
-  Printf.printf "plotting end common from %d to %d\n" n (Array.length mine - 1);
   Array.fold_right (plot_elt Idem) (Array.sub mine n (Array.length mine - n)) ctx
 
 let delete_file fnl =
@@ -191,50 +181,45 @@ let delete_file fnl =
 let build_svg_name aprefix aschname =
   aprefix ^ (String.sub aschname 0 (String.length aschname - 4)) ^ ".svg"
 
-let context_from fs = find_libs fs >>= read_libs (initial_context ()) fs
-let list_context fs = find_libs fs >>= read_libs_l (L.initial_context ()) fs
+module type Differ =  sig
+  type pctx
+  module S : SchPainter with type painterContext = pctx
+  val display_diff: pctx -> pctx -> string -> unit Lwt.t
+end
 
-
-let doit_internal from_fs to_fs =
-  let from_context = list_context from_fs in
-  let to_context = list_context to_fs in
-  let from_list = find_schematics from_fs in
-  let to_list = find_schematics to_fs in
-  let file_list = intersect_lists from_list to_list in
-  let display_diff filename =
-    let svg_name = filename ^".svg" in
-    let get_canevas fs context =
-          let module M = (val fs: Simple_FS) in
-          let content = M.get_content[filename] in
-          context_list context content
-    in
-    get_canevas from_fs from_context >>= fun from_canevas ->
-    get_canevas to_fs to_context >>= fun to_canevas ->
+module InternalDiff = struct
+  type pctx = ListPainter.listcanevas
+  module S = LP
+  let display_diff from_ctx to_ctx filename =
+    let from_canevas = Array.of_list from_ctx in
+    let to_canevas = Array.of_list to_ctx in
+    let svg_name = build_svg_name "diff_" filename in
     let outctx = draw_difftotal from_canevas to_canevas (SvgPainter.get_context ()) in
-    Lwt_io.with_file ~mode:Lwt_io.Output svg_name (fun o -> Lwt_io.write o (SvgPainter.write outctx))
-  in
-  let compare_all = file_list >>= (Lwt_list.map_p display_diff) >|= to_unit in
-  Lwt_main.run compare_all
+    Lwt_io.with_file ~mode:Lwt_io.Output svg_name (fun o -> Lwt_io.write o (SvgPainter.write outctx)) >>= fun _ ->
+    Lwt_process.exec ("", [| "chromium"; svg_name|]) >|= to_unit >>= fun _ ->
+    delete_file svg_name
+end
 
-let doit from_fs to_fs =
-  let from_context = context_from from_fs in
-  let to_context = context_from to_fs in
-  let from_list = find_schematics from_fs in
-  let to_list = find_schematics to_fs in
-  let file_list = intersect_lists from_list to_list in
-  let display_diff filename =
+module SP = struct
+  include S
+  type painterContext = SvgPainter.t
+end
+
+module ImageDiff = struct
+  type pctx = SvgPainter.t
+  module S = SP
+  let display_diff from_ctx to_ctx filename =
     let from_filename = build_svg_name "from_" filename in
     let to_filename = build_svg_name "to_" filename in
     let both_files =
       List.map (
-          fun (fs, svg_name, context) ->
-          let module M = (val fs: Simple_FS) in
-          let content = M.get_content [filename] in
-          process_file context svg_name content) [(from_fs, from_filename, from_context); (to_fs, to_filename, to_context)] in
+        fun (svg_name, context) -> Lwt_io.with_file ~mode:Lwt_io.Output svg_name (fun o ->
+            Lwt_io.write o (SvgPainter.write context)))
+        [(from_filename, from_ctx); (to_filename, to_ctx)] in
     let both = Lwt.join both_files in
     let compare_them =
       both >>= fun _ ->
-      Lwt_process.exec ("", [| "git-imgdiff"; from_filename ; to_filename|]) >|= to_unit in
+       Lwt_process.exec ("", [| "git-imgdiff"; from_filename ; to_filename|]) >|= to_unit in
     Lwt.join @@
       List.map (fun f ->
           Lwt.catch
@@ -247,20 +232,37 @@ let doit from_fs to_fs =
              | InternalGitError s -> Printf.printf "%s\n" s
              | _ -> Printf.printf "unknown error\n"
               end; delete_file f))
-               [from_filename; to_filename] in
-  let compare_all = file_list >>= (Lwt_list.map_p display_diff) >|= to_unit in
+        [from_filename; to_filename]
+end
+
+let doit from_fs to_fs differ =
+  let module D = (val differ : Differ) in
+  let module F = (val from_fs: Simple_FS) in
+  let module T = (val to_fs: Simple_FS) in
+  let module FromP = FSPainter (D.S) (F) in
+  let module ToP = FSPainter (D.S) (F) in
+  let from_list = FromP.find_schematics () in
+  let to_list = ToP.find_schematics () in
+  let file_list = intersect_lists from_list to_list in
+  let from_ctx = FromP.context_from () in
+  let to_ctx = ToP.context_from () in
+  let compare_one filename =
+    FromP.process_file from_ctx filename >>= fun from_endctx ->
+    ToP.process_file to_ctx filename >>= fun to_endctx ->
+    D.display_diff from_endctx to_endctx filename in
+  let compare_all =  file_list >>= Lwt_list.map_p compare_one >|= to_unit in
   Lwt_main.run compare_all
 
 let () =
   match Array.length Sys.argv with
   | 1 -> let from_ref = rev_parse "HEAD" in
-        doit_internal (git_fs from_ref) (true_fs ".")
+        doit (git_fs from_ref) (true_fs ".") (module ImageDiff : Differ)
   | 2 ->
     let from_ref = rev_parse Sys.argv.(1) in
-    doit_internal (git_fs from_ref) ( true_fs ".")
+    doit (git_fs from_ref) ( true_fs ".") (module InternalDiff: Differ)
   | 3 ->
      let from_ref = rev_parse Sys.argv.(1) in
      let to_ref = rev_parse Sys.argv.(2) in
-     doit (git_fs from_ref) (git_fs to_ref)
+     doit (git_fs from_ref) (git_fs to_ref) (module InternalDiff: Differ)
   | _ ->
     Printf.printf "%s needs 0, 1 or 2 revs to compare\n" Sys.argv.(0); exit 3
