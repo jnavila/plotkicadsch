25c4ee9ef4edb4383209d38b580dfec3663c80a3
Jean-Noël Avila <jn.avila@free.fr>
2018-10-27 23:16:36 +0200

    fix documentation strings

--- kicadsch/src/kicadSch_sigs.mli
+++ kicadsch/src/kicadSch_sigs.mli
@@ -35,39 +35,47 @@ module type Painter = sig
   val paint_text: ?kolor: kolor -> String.t -> orientation -> coord -> size -> justify -> style -> t -> t
   (** [paint ?kolor text orient coord size justification style canvas]
      adds a [text] with the given [orient], [size], [justification]
-     and [style] at the given [coord] to [canvas].@return the modified canvas *)
+     and [style] at the given [coord] to [canvas].
+     @return the modified canvas *)
 
   val paint_line: ?kolor: kolor -> ?width: size -> coord -> coord -> t -> t
   (** [paint_line ?kolor width start end canvas] paints a line with
      the given [kolor] and [width] between [start] and [stop] on
-     [canvas]. @return the modified canvas *)
+     [canvas].
+     @return the modified canvas *)
 
   val paint_circle: ?kolor: kolor -> ?fill: kolor -> coord -> int -> t -> t
   (** [paint_circle ?kolor center radius canvas] paints a circle
      filled with the given [kolor] defined by [center] and [radius] on
-     [canvas]. @return the modified canvas *)
+     [canvas].
+     @return the modified canvas *)
 
   val paint_rect: ?kolor: kolor -> ?fill: kolor -> coord -> coord -> t -> t
   (** [paint_rect ?kolor corner1 corner2 canvas] paints a rectangle
      filled with the given [kolor] defined by [corner1] and [corner2]
-     on [canvas]. @return the modified canvas *)
+     on [canvas].
+     @return the modified canvas *)
 
   val paint_image: coord -> float -> Buffer.t -> t -> t
   (** [paint_image corner scale png canvas] paints a [png] image
-     filled at [corner], scaled at [scale] on [canvas]. @return the
+     filled at [corner], scaled at [scale] on [canvas].
+     @return the
      modified canvas *)
 
   val paint_arc: ?kolor: kolor -> ?fill: kolor -> coord -> coord -> coord -> int -> t -> t
   (** [paint_arc ?kolor center start end radius canvas] paints an arc filled
      with [kolor] between [start] and [end] of [radius] around center on
-     [canvas]. @return the modified canvas *)
+     [canvas].
+     @return the modified canvas *)
 
   val set_canevas_size: int -> int -> t -> t
   (** [set_canevas x y canvas] set the size of the canevas
-      @return the modified canvas *)
+
+     @return the modified canvas *)
 
   val get_context: unit -> t
-  (** [get_context ()] @return a new painting canvas *)
+  (** [get_context ()]
+     @return a new painting canvas *)
 
 end
 
@@ -81,16 +89,19 @@ module type SchPainter = sig
   (** the underlying context *)
 
   val initial_context : unit -> schContext
-  (** [initial_context ()] @return an new empty context *)
+  (** [initial_context ()]
+     @return an new empty context *)
 
   val add_lib: string -> schContext -> schContext
   (** [add_lib line context] parse the content of [line] provided to
-     libs to the [context]. @return the updated context *)
+     libs to the [context].
+     @return the updated context *)
 
   val parse_line :
     String.t -> schContext -> schContext
   (** [parse_line line context] parse a new [line] of schematic and
-     update [context]. @return the updated context *)
+     update [context].
+     @return the updated context *)
 
   val output_context: schContext -> painterContext
   (** [output_context context output] write the [context] as a image
@@ -112,17 +123,19 @@ sig
   (** A drawing context *)
 
   val lib: unit -> t
-  (** [lib ()] @return an empty new component manager *)
+  (** [lib ()]
+     @return an empty new component manager *)
 
   val append_lib: string -> t -> t
   (** [append_lib stream context] appends the lib contained in the
-     [stream] to the context. @return the updated context *)
+     [stream] to the context.
+     @return the updated context *)
 
   val plot_comp: t -> string -> int -> coord -> transfo -> drawContext -> drawContext * bool
   (** [plot_comp lib name partnumber origin transformation context]
      find in [lib] the component with given [name] and plot the part
      [partnumber] at [origin] after [transfomation] into the graphical
-     [context] and the fact that the component is multipart. @return
-     the updated graphical context *)
+     [context] and the fact that the component is multipart.
+     @return the updated graphical context *)
 
 end
